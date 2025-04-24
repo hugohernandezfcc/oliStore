b6273c4492a5e2cdfa12e40cd78795f70ba2b316
Hugo Hernández <hug.hernandez@arrow.com>
2025-04-24 00:24:10 -0600

    migrations

---
+++ database/migrations/2025_04_24_001912_create_contact_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname')->nullable();
+            $table->string('lastname')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('prefer_channel')->nullable();
+            $table->string('prefer_time')->nullable();
+            $table->unsignedBigInteger('account_id')->nullable();
+            $table->foreign('account_id')->references('id')->on('account')->restrictOnDelete();
+            $table->string('status')->default('active');
+
+            $table->unsignedBigInteger('created_at')->nullable();
+            $table->foreign('created_at')->references('id')->on('users')->restrictOnDelete();
+
+            $table->unsignedBigInteger('updated_at')->nullable();
+            $table->foreign('updated_at')->references('id')->on('users')->restrictOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact');
+    }
+};
